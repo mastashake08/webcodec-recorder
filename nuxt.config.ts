16af5a62ca4eb63202ffd5185a64f103f533d42e
Jyrone Parker <jyrone.parker@atlasied.com>
2024-07-07 12:29:34 -0400

    pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,45 @@ export default defineNuxtConfig({
   },
 
   modules: ['@vite-pwa/nuxt'],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'WebCodec Recorder',
+      short_name: 'WCR',
+      theme_color: '#000000',
+      "background_color": "black",
+
+      "display": "standalone",
+      "categories": ["entertainment", "movies"],
+      "display_override": ["fullscreen", "window-controls-overlay"],
+      "launch_handler": {
+        "client_mode": "focus-existing"
+    },
+    "description": "A PWA using Video Frames and WebCodecs",
+
+      icons: [
+        {
+          src: '/webcodec-recorder/hacker.svg',
+          sizes: '512x512',
+          type: 'image/svg',
+          purpose: 'any maskable',
+        },
+      ]
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  }
 })
