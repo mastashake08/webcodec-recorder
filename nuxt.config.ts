e74bb822939e3a9392fc7603d68d146da2fba97a
Jyrone Parker <jyrone.parker@atlasied.com>
2024-07-07 11:53:08 -0400

    pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  ssr: false
+  ssr: false,
+
+  app: {
+    baseURL: '/webcodec-recorder/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
+
+  modules: ['@vite-pwa/nuxt'],
 })
